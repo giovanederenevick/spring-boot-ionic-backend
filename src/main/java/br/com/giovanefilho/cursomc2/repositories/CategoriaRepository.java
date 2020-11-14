9d58e909dd357845a0652a15d38545bea5308f72
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2020-11-13 21:41:10 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/br/com/giovanefilho/cursomc2/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package br.com.giovanefilho.cursomc2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.giovanefilho.cursomc2.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
+}
