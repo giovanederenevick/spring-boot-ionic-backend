88d5ee3e908279f8843bb4c8a99d1c006a4d4f1f
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2020-11-15 21:07:44 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/br/com/giovanefilho/cursomc2/services/CategoriaService.java
+++ src/main/java/br/com/giovanefilho/cursomc2/services/CategoriaService.java
@@ -24,4 +24,10 @@ public class CategoriaService {
 				+ Categoria.class.getName()
 				));
 	}
+	
+	public Categoria insert(Categoria obj) {
+		
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
