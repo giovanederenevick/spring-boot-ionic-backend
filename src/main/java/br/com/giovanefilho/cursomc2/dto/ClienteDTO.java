cd5b7944f61a7c5adcd3ea9ace85768a2b72631a
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2020-11-16 12:25:41 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/br/com/giovanefilho/cursomc2/dto/ClienteDTO.java
+++ src/main/java/br/com/giovanefilho/cursomc2/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.giovanefilho.cursomc2.domain.Cliente;
+import br.com.giovanefilho.cursomc2.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
