f6e6cfb69a11c17d62fda23586d64e060ee20f22
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2021-02-22 18:58:01 -0300

    Email HTML

--- src/main/java/br/com/giovanefilho/cursomc2/services/EmailService.java
+++ src/main/java/br/com/giovanefilho/cursomc2/services/EmailService.java
@@ -1,5 +1,7 @@
 package br.com.giovanefilho.cursomc2.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import br.com.giovanefilho.cursomc2.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
